ba7c5bb1e240100c5d8a19d7f1b179570476c8df
SyAtlantis <1102960695@qq.com>
2020-01-03 19:25:12 +0800

    数据坐标

--- src/bills/Pdfkit/Pdfkit.js
+++ src/bills/Pdfkit/Pdfkit.js
@@ -15,14 +15,62 @@ class Pdfkit extends React.Component {
       const doc = new PDFDocument({ autoFirstPage: false })
       doc.addPage({ layout: 'landscape', size: [600, 800] })
       const stream = doc.pipe(BlobStream())
-      // doc.fontSize(25).text('Here is some vector graphics...', 100, 100)
-      // var img = require('./logo.png')
       doc.image(invoiceBgBs64, {
         fit: [800, 600],
         align: 'center',
         valign: 'center'
       })
-      doc.fontSize(15).text('12333444', 120, 200)
+
+      doc.fontSize(15).text('12333444', 100, 138)
+      doc.fontSize(15).text('12333444', 100, 160)
+      doc.fontSize(15).text('12333444', 550, 138)
+      doc.fontSize(15).text('12333444', 550, 160)
+
+      doc.fontSize(15).text('12333444', 130, 188)
+      doc.fontSize(15).text('12333444', 130, 210)
+      doc.fontSize(15).text('12333444', 130, 232)
+      doc.fontSize(15).text('12333444', 480, 188)
+      doc.fontSize(15).text('12333444', 480, 210)
+      doc.fontSize(15).text('12333444', 480, 232)
+
+      doc.fontSize(15).text('12333444', 193, 254)
+      doc.fontSize(15).text('12333444', 455, 254)
+      doc.fontSize(15).text('12333444', 670, 254)
+
+      doc.fontSize(15).text('222222', 190, 288)
+      doc.fontSize(15).text('9', 500, 300)
+      doc.fontSize(15).text('8', 522, 300)
+      doc.fontSize(15).text('7', 544, 300)
+      doc.fontSize(15).text('6', 566, 300)
+      doc.fontSize(15).text('5', 587, 300)
+      doc.fontSize(15).text('4', 608, 300)
+      doc.fontSize(15).text('3', 630, 300)
+      doc.fontSize(15).text('2', 651, 300)
+      doc.fontSize(15).text('1', 672, 300)
+      doc.fontSize(15).text('9', 695, 300)
+      doc.fontSize(15).text('8', 716, 300)
+      doc.fontSize(15).text('7', 736, 300)
+
+      doc.fontSize(15).text('12333444', 206, 325)
+      doc.fontSize(15).text('12333444', 206, 348)
+      doc.fontSize(15).text('12333444', 480, 325)
+      doc.fontSize(15).text('12333444', 480, 348)
+
+      doc.fontSize(15).text('12333444', 130, 369)
+      doc.fontSize(15).text('12333444', 130, 400)
+      doc.fontSize(15).text('12333444', 670, 414)
+
+      doc.fontSize(15).text('12333444', 190, 435)
+      doc.fontSize(15).text('12333444', 450, 435)
+      doc.fontSize(15).text('12333444', 670, 435)
+
+      doc.fontSize(15).text('12333444', 262, 458)
+      doc.fontSize(15).text('12333444', 542, 458)
+      doc.fontSize(15).text('12333444', 684, 458)
+      doc.fontSize(15).text('12333444', 262, 482)
+      doc.fontSize(15).text('12333444', 542, 482)
+      doc.fontSize(15).text('12333444', 684, 482)
+
       doc.end()
       stream.on('finish', () => {
         const url = stream.toBlobURL('application/pdf')
--- src/bills/types/001form.js
+++ src/bills/types/001form.js
@@ -3,32 +3,75 @@ import { translate } from 'react-i18next'
 // import { connect } from 'redux-bundler-react'
 
 class MyForm extends React.Component {
+  state = {
+    issue_date: '1111',
+    bill_due_date: '',
+    bill_state: '',
+    bill_no: '',
+    drawer_name: '',
+    drawer_account: '',
+    drawer_bank: '',
+    payee_name: '',
+    payee_account: '',
+    payee_bank: '',
+    bail_name: '',
+    bail_address: '',
+    bail_date: '',
+    money: '',
+    acceptor_name: '',
+    acceptor_account: '',
+    acceptor_bank_no: '',
+    acceptor_bank_name: '',
+    contract_no: '',
+    is_transfer: '',
+    acceptor_date: '',
+    drawer_rate: '',
+    drawer_credit: '',
+    drawer_due_date: '',
+    acceptor_rate: '',
+    acceptor_credit: '',
+    acceptor_due_date: ''
+  }
+
+  handleChange = (key, event) => {
+    let form = this.state.form
+    for (let item in this.state.form) {
+      if (item === key) {
+        form[item] = event.target.value
+        this.setState({ form: form })
+      }
+    }
+  }
   render () {
     return (
       <div>
-        <div>出票日期：<input/></div>
-        <div>汇票到期日：<input/></div>
-        <div>票据状态：<input/></div>
-        <div>票据号码：<input/></div>
-        <div>出票人 全称：<input/></div>
-        <div>出票人 账号：<input/></div>
-        <div>出票人 开户银行：<input/></div>
-        <div>开票人 全称：<input/></div>
-        <div>开票人 账号：<input/></div>
-        <div>开票人 开户银行：<input/></div>
-        <div>保证人名称：<input/></div>
-        <div>保证人地址：<input/></div>
-        <div>保证日期：<input/></div>
-        <div>人民币(大写)：<input/></div>
-        <div>保证日期：<input/></div>
-        <div>保证日期：<input/></div>
-        <div>保证日期：<input/></div>
-        <div>保证日期：<input/></div>
-        <div>保证日期：<input/></div>
-        <div>保证日期：<input/></div>
-        <div>保证日期：<input/></div>
-        <div>保证日期：<input/></div>
-        <div>保证日期：<input/></div>
+        <div><span>出票日期：</span><input type="text" onChange={this.handleChange.bind(this, 'issue_date')}/></div>
+        <div>汇票到期日：<input type="text" onChange={this.handleChange.bind(this, 'bill_due_date')}/></div>
+        <div>票据状态：<input type="text" onChange={this.handleChange.bind(this, 'bill_state')}/></div>
+        <div>票据号码：<input type="text" onChange={this.handleChange.bind(this, 'bill_no')}/></div>
+        <div>出票人 全称：<input type="text" onChange={this.handleChange.bind(this, 'drawer_name')}/></div>
+        <div>出票人 账号：<input type="text" onChange={this.handleChange.bind(this, 'drawer_account')}/></div>
+        <div>出票人 开户银行：<input type="text" onChange={this.handleChange.bind(this, 'drawer_bank')}/></div>
+        <div>收票人 全称：<input type="text" onChange={this.handleChange.bind(this, 'payee_name')}/></div>
+        <div>收票人 账号：<input type="text" onChange={this.handleChange.bind(this, 'payee_account')}/></div>
+        <div>收票人 开户银行：<input type="text" onChange={this.handleChange.bind(this, 'payee_bank')}/></div>
+        <div>保证人名称：<input type="text" onChange={this.handleChange.bind(this, 'bail_name')}/></div>
+        <div>保证人地址：<input type="text" onChange={this.handleChange.bind(this, 'bail_address')}/></div>
+        <div>保证日期：<input type="text" onChange={this.handleChange.bind(this, 'bail_date')}/></div>
+        <div>人民币：<input type="text" onChange={this.handleChange.bind(this, 'money')}/></div>
+        <div>承兑人 全称：<input type="text" onChange={this.handleChange.bind(this, 'acceptor_name')}/></div>
+        <div>承兑人 账号：<input type="text" onChange={this.handleChange.bind(this, 'acceptor_account')}/></div>
+        <div>承兑人 开户行行号：<input type="text" onChange={this.handleChange.bind(this, 'acceptor_bank_no')}/></div>
+        <div>承兑人 开户行名称：<input type="text" onChange={this.handleChange.bind(this, 'acceptor_bank_name')}/></div>
+        <div>交易合同号：<input type="text" onChange={this.handleChange.bind(this, 'contract_no')}/></div>
+        <div>能否转让：<input type="text" onChange={this.handleChange.bind(this, 'is_transfer')}/></div>
+        <div>承兑日期：<input type="text" onChange={this.handleChange.bind(this, 'acceptor_date')}/></div>
+        <div>出票人 评级主体：<input type="text" onChange={this.handleChange.bind(this, 'drawer_rate')}/></div>
+        <div>出票人 信用等级：<input type="text" onChange={this.handleChange.bind(this, 'drawer_credit')}/></div>
+        <div>出票人 评级到期日：<input type="text" onChange={this.handleChange.bind(this, 'drawer_due_date')}/></div>
+        <div>承兑人 评级主体：<input type="text" onChange={this.handleChange.bind(this, 'acceptor_rate')}/></div>
+        <div>承兑人 信用等级：<input type="text" onChange={this.handleChange.bind(this, 'acceptor_credit')}/></div>
+        <div>承兑人 评级到期日：<input type="text" onChange={this.handleChange.bind(this, 'acceptor_due_date')}/></div>
       </div>
     )
   }
